6945376c4942426cf047ff0c1e00e67ba8ccb045
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-08 23:54:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,11 +1,23 @@
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each do |shopping|
+    shopping.each do |veggies, sales|
+      new_hash[veggies] ||= sales
+      new_hash[veggies][:count] ||= 0
+      new_hash[veggies][:count] += 1
+    end
+  end
+  new_hash # code here
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  cart.each do |item, item_properties|
+    item_properties.each do |key, value|
+      puts coupon.keys
+    # if coupon.each {|coupon_k, coupon_v|}
+    end
+  end
 end
-
 def apply_clearance(cart)
   # code here
 end
